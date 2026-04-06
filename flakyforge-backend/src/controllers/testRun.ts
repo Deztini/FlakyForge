1bf62dac000f8441ec02e1561162aeee1b22bda6
Deztini <agbatordestiny26@gmail.com>
2026-04-06 14:40:54 +0100

    feat: Add test run controller

---
+++ flakyforge-backend/src/controllers/testRun.ts
@@ -0,0 +1,62 @@
+import { NextFunction, Request, Response } from "express";
+import z from "zod";
+import type { IUser } from "../models/User";
+import { RepoService } from "../services/repoService";
+import { ApiError } from "../utils/ApiError";
+
+const collectResultsSchema = z.object({
+  githubRepoId: z.number(),
+  repoFullName: z.string(),
+  results: z.array(
+    z.object({
+      name: z.string(),
+      failRate: z.number(),
+      runs: z.number(),
+      isFlaky: z.boolean(),
+    }),
+  ),
+});
+
+export const TestRunController = {
+  async triggerScan(
+    req: Request<{ repoId: string }>,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const user = req.user as IUser;
+
+      const testRun = await RepoService.triggerScan(req.params.repoId, user);
+
+      res.status(201).json({
+        message: "Scan triggered successfully",
+        testRun,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async collectResults(
+    req: Request<{ repoId: string }>,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const apiKey = req.headers["x-api-key"] as string;
+
+      if (!apiKey) throw ApiError.unauthorized("Missing API key");
+
+      const payload = collectResultsSchema.parse(req.body);
+
+      const testRun = await RepoService.collectResults(apiKey, payload);
+
+      res.status(201).json({
+        message: "Results collected successfully",
+        testRun,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
