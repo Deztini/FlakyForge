a8317d630618f7b648e255873540fb3e81c955a3
Deztini <agbatordestiny26@gmail.com>
2026-04-22 15:18:38 +0100

    feat: Add get test run and get metrics to the test run controller

--- flakyforge-backend/src/controllers/testRun.ts
+++ flakyforge-backend/src/controllers/testRun.ts
@@ -2,11 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import z from "zod";
 import type { IUser } from "../models/User";
 import { RepoService } from "../services/repoService";
+import { TestRunService } from "../services/testRunService";
 import { ApiError } from "../utils/ApiError";
 
 const collectResultsSchema = z.object({
   githubRepoId: z.number(),
   repoFullName: z.string(),
+  commitSha: z.string(),
+  totalTests: z.number(),
   results: z.array(
     z.object({
       id: z.string(),
@@ -15,7 +18,7 @@ const collectResultsSchema = z.object({
       testCode: z.string(),
       runs: z.number(),
       isFlaky: z.boolean(),
-      file: z.string()
+      file: z.string(),
     }),
   ),
 });
@@ -58,6 +61,34 @@ export const TestRunController = {
         message: "Results collected successfully",
         testRun,
       });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async getTestRuns(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = req.user as IUser;
+      const page = Number(req.query.page) || 1;
+      const limit = Math.min(Math.max(Number(req.query.limit) || 10, 1), 50);
+
+      const result = await TestRunService.getTestRuns(
+        user._id.toString(),
+        page,
+        limit,
+      );
+
+      res.status(200).json(result);
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async getMetrics(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = req.user as IUser;
+      const metrics = await TestRunService.getMetrics(user._id.toString());
+      res.status(200).json(metrics);
     } catch (err) {
       next(err);
     }
