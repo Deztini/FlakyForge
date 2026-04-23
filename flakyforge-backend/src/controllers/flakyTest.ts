bf6213ac6836dda79246fc77dbabf51ef542ac50
Deztini <agbatordestiny26@gmail.com>
2026-04-23 14:29:33 +0100

    feat: Add get flaky tests to flakytest controller

---
+++ flakyforge-backend/src/controllers/flakyTest.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Response, Request } from "express";
+import { IUser } from "../models/User";
+import { FlakyTestService } from "../services/flakyTestService";
+
+export const FlakyTestController = {
+  async getFlakyTests(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = req.user as IUser;
+      const page = Number(req.query.page) || 1;
+      const limit = Math.min(Math.max(Number(req.query.limit) || 10, 1), 50);
+
+      const statusFilter = req.query.status as
+        | "unfixed"
+        | "pending"
+        | "fixed"
+        | undefined;
+
+      const result = await FlakyTestService.getFlakyTests(
+        user._id.toString(),
+        page,
+        limit,
+        statusFilter,
+      );
+
+      res.status(200).json({
+        success: true,
+        message: "Flaky tests fetched successfully",
+        data: result,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
