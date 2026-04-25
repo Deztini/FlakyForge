e64e9e326323e5634e319711608fc874d210b9aa
Deztini <agbatordestiny26@gmail.com>
2026-04-25 17:56:27 +0100

    feat: Add get dashboard summary to the dashboard controller

---
+++ flakyforge-backend/src/controllers/dashboard.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { IUser } from "../models/User";
+import { DashboardService } from "../services/dashboardService";
+
+export const DashboardController = {
+  async getDashboardSummary(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = req.user as IUser;
+
+      const result = await DashboardService.getSummary(user._id.toString());
+
+      return res.status(200).json({
+        success: true,
+        message: "Dashboard summary fetched successfully",
+        data: result,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
