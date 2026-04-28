70e425b188bab9b2ddb87c5803fddaca85335725
Deztini <agbatordestiny26@gmail.com>
2026-04-28 14:57:44 +0100

    feat: Add flaky test page

---
+++ flakyforge-frontend/src/features/flaky-tests/pages/FlakyTestsPage.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { AuthGuard } from "../../../components/guards/AuthGuard";
+import { useFlakyTests, useFlakyTestMetrics } from "../../../hooks/useFlakyTests";
+import { FlakyTestsStatCards } from "../components/FlakyTestsStatCards";
+import { FlakyTestsTable } from "../components/FlakyTestsTable/FlakyTestsTable";
+import type { FlakyTestStatus } from "../../../api/flakyTestApi";
+
+export function FlakyTestsPage() {
+  const [activeFilter, setActiveFilter] = useState<FlakyTestStatus | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const {
+    data: metrics,
+    isLoading: metricsLoading,
+    isError: metricsError,
+  } = useFlakyTestMetrics();
+
+  const {
+    data: flakyTestsData,
+    isLoading: testsLoading,
+    isError: testsError,
+  } = useFlakyTests(currentPage, 10, activeFilter);
+
+
+  function handleFilterChange(filter: FlakyTestStatus | undefined) {
+    setActiveFilter(filter);
+    setCurrentPage(1);
+  }
+
+  return (
+    <AuthGuard>
+      <div className="space-y-6">
+        <FlakyTestsStatCards
+          isLoading={metricsLoading}
+          isError={metricsError}
+          metrics={metrics}
+        />
+
+        <FlakyTestsTable
+          isLoading={testsLoading}
+          isError={testsError}
+          data={flakyTestsData}
+          metrics={metrics}
+          activeFilter={activeFilter}
+          onFilterChange={handleFilterChange}
+          onPageChange={setCurrentPage}
+        />
+      </div>
+    </AuthGuard>
+  );
+}
