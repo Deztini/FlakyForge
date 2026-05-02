be71046b7893cace06239112626b619b96370fea
Deztini <agbatordestiny26@gmail.com>
2026-05-02 14:08:53 +0100

    Remove dist folder from tracking again

--- flakyforge-backend/dist/routes/flakyTest.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authenticate_1 = require("../middleware/authenticate");
-const flakyTest_1 = require("../controllers/flakyTest");
-const router = (0, express_1.Router)();
-router.get("/", authenticate_1.authenticate, flakyTest_1.FlakyTestController.getFlakyTests);
-router.get("/metrics", authenticate_1.authenticate, flakyTest_1.FlakyTestController.getFlakyTestMetrics);
-exports.default = router;
