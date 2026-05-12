6e606cc79ac7f4912446dec1e255c90837f29742
Deztini <agbatordestiny26@gmail.com>
2026-05-12 10:27:24 +0100

    Add Toaster component to the root file

--- flakyforge-frontend/src/main.tsx
+++ flakyforge-frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen.ts";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+import { Toaster } from "sonner";
 
 const router = createRouter({
   routeTree,
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster theme="dark" position="bottom-right" richColors />
     </QueryClientProvider>
   </StrictMode>,
 );
